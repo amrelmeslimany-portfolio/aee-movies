9c04776be8a5041266cfd9bb05191f5a728b78b7
amrelmeslimany-portfolio <aamr_786@hotmail.com>
2023-07-13 14:07:22 +0300

    Hosting MongoDB

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const mongoURL = process.env.MONGO_DB;
 
 mongoose.Promise = global.Promise;
+
 module.exports = mongoose
   .connect(mongoURL)
-  .then(() => console.log("DB Successfull"))
+  .then(() => {
+    console.log("DB Successfull");
+  })
   .catch((error) => console.log(error));
